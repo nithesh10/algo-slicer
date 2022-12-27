95dc7e67bd03aff912371214e2534321d33fc427
nithesh10 <a.nitheshkumar@gmail.com>
2022-12-27 10:29:37 +0530

    changes

--- creds.py
+++ creds.py
@@ -4,6 +4,7 @@ PWD = ['Sv@30million','QWERTY@12345']
 API_KEY = ['AYq866MN','clBgCN8g']
 ACCESS_TOKEN=['af075e7a-269e-4a82-9ce2-d9bcdd073ea0','a617b459-7250-4030-ab36-a37e55767551']
 qrOtp=['WURWJUZIS6MMZGOI2POUVAWDVY','NY3743YUYKMI2B6LZJJD6VTDSI']
+
 # Don't change  below variable
 FEED_TOKEN = []
 TOKEN_MAP = []
@@ -17,4 +18,4 @@ import pandas as pd
 token_info = pd.DataFrame()
 trade_list = [[]*len(USER_NAME)]
 orderbook=[]
-version_code=10
+version_code=12
